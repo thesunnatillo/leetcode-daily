f3db7d483a4bf66d164aa5f1328dee2c12ec83fb
thesunnatillo <sunnatillocoder@gmail.com>
2024-10-29 21:06:23 +0500

    update

--- javascript/palindrome-number.js
+++ javascript/palindrome-number.js
@@ -3,5 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-
+    let x_str = String(x)
+    if (x >= 0) {
+        return x_str.split('').reverse().join('') === x_str;
+    } else {
+        return false
+    }
 };
