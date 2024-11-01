c104a41b8426c72bbe96c73248e19c1d6d3f54c7
thesunnatillo <sunnatillocoder@gmail.com>
2024-11-01 09:54:23 +0500

    update

--- javascript/longest-common-prefix.js
+++ javascript/longest-common-prefix.js
@@ -3,8 +3,15 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
 
-    let count = 0;
-    
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
+        }
+    }
+    return prefix;
 };
-
